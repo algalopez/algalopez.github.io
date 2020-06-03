8a534bac52a2bc2a4cb3a7ab7dfbe64a0a79482a
algalopez <algalopez85@gmail.com>
2020-06-03 22:08:19 +0200

    Issue-0 Add javascipt files

---
+++ script/listener.js
@@ -0,0 +1,27 @@
+/*
+        _             _                      
+       | |           | |                     
+   __ _| | __ _  __ _| | ___  _ __   ___ ____
+  / _` | |/ _` |/ _` | |/ _ \| '_ \ / _ \_  /
+ | (_| | | (_| | (_| | | (_) | |_) |  __// / 
+  \__,_|_|\__, |\__,_|_|\___/| .__/ \___/___|
+           __/ |             | |             
+          |___/              |_|             
+*/
+
+const STATE_LOADING = "loading"
+const STATE_INTERACTIVE = "interactive"
+const STATE_COMPLETE = "complete"
+
+if (document.readyState == STATE_LOADING) {
+    document.addEventListener('DOMContentLoaded', listener_start);
+} else {
+    listener_start();
+}
+
+function listener_start() {
+    console.log('Web loaded')
+
+    executeButton = document.getElementById("execute");
+    executeButton.addEventListener('click', script_executeQuery);
+}
---
+++ script/script.js
@@ -0,0 +1,5 @@
+function script_executeQuery() {
+
+    var queryTxt = document.getElementById("query").value;
+    console.log("Executing query: %s", queryTxt);
+}
